b5dbf44e93978b3b2d019b5139a9c6bb746a8296
KoenHaemels <koenhaemels@gmail.com>
2017-04-25 09:50:27 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #Main script
-sudo chmod +x {{ .Path }}; {{ .Vars }} sudo -E sh '{{ .Path }}'
 sudo apt-get update && sudo apt-get upgrade -y
-
-/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
-
 #Installation apache2
 sudo apt-get install apache2 -y
 #Installation php
@@ -12,3 +8,7 @@ sudo apt-get install php libapache2-mod-php -y
 #Clear any docs in /html and copy docs from Application into /html
 sudo rm -f /var/www/html/* -r
 sudo cp /var/ApplicationOnly/WorkingSite/* /var/www/html/ -r -f
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo chmod +x {{ .Path }}; {{ .Vars }} sudo -E sh '{{ .Path }}'
+/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
